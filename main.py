cb0708587593561d8062eb88d883d54e0717caf2
sativiensiri <35590263+sativiensiri@users.noreply.github.com>
2023-05-12 01:10:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, url_for, request, redirect
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+  name = "Sati Viensiri"
+  print(f"Hello Mr.{name}")
+  return render_template("index.html")
+
+@app.route("/login", methods=['POST'])
+def login():
+  if request.method == 'POST':
+      email = request.form.get('email')
+      password = request.form.get('password')
+      if email != "" and password != "":
+        print(f"We have {email} and {password}")
+        return render_template("register_page.html")
+      else:
+        return redirect(url_for('index'))
+      
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
